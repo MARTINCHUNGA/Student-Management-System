ad84ebff272a7f7d7a021dc8161eb29d9b9c328d
MARTINCHUNGA <Bsc-77-16@cc.ac.mw>
2022-09-20 00:41:11 +0200

    created student model

---
+++ Student-Management-System-Backend/src/main/java/com/example/StudentManagementSystemBackend/Models/Student.java
@@ -0,0 +1,38 @@
+package com.example.StudentManagementSystemBackend.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+
+
+@Data 
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "student_id")
+    private int id;
+
+    @Column(name = "firstName")
+    @NotEmpty(message = "Please enter your First Name")
+    private String firstName;
+
+    @Column(name = "lastName")
+    @NotEmpty(message = "Please enter your Last Name")
+    private String lastName;
+
+    @Email(message = "Please enter valid Email Address")
+    @Column(name = "email")
+    @NotEmpty(message = "Please enter your Email Address")
+    private String email;
+    
+}
