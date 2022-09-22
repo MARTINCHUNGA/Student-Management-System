3abce66faee434dc773577b059e6cafc89090433
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2022-09-22 23:10:14 +0200

    structuring

--- Student-Management-System-Backend/src/main/java/com/example/StudentManagementSystemBackend/employe/Employee.java
+++ Student-Management-System-Backend/src/main/java/com/example/StudentManagementSystemBackend/employe/Employee.java
@@ -1,8 +1,11 @@
 package com.example.StudentManagementSystemBackend.employe;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import javax.validation.constraints.*;
 
+@Data
 @Entity
 @Table(name = "Employees")
 public class Employee {
@@ -43,90 +46,4 @@ public class Employee {
 
     @Column(name = "rank")
     private String rank;
-
-
-    public Employee() {
-    }
-
-    public Employee(int id,
-                    String firstName,
-                    String lastName,
-                    String sex,
-                    String address,
-                    String email,
-                    String password,
-                    String rank) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.sex = sex;
-        this.address = address;
-        this.email = email;
-        this.password = password;
-        this.rank = rank;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getRank() {
-        return rank;
-    }
 }
--- Student-Management-System-Backend/src/main/java/com/example/StudentManagementSystemBackend/student/Student.java
+++ Student-Management-System-Backend/src/main/java/com/example/StudentManagementSystemBackend/student/Student.java
@@ -34,36 +34,5 @@ public class Student {
     @Column(name = "email")
     @NotEmpty(message = "Please enter your Email Address")
     private String email;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    
 }
